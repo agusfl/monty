a0241a17becf20bda35433134a45a8e79768870b
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-11 22:54:55 +0000

    add changes in rotl

--- rotl.c
+++ rotl.c
@@ -6,23 +6,20 @@
  *@line_number: line
  **/
 
-void _rotl(stack_t **head, unsigned int line_number)
+void _rotl(stack_t **head, unsigned int __attribute__((unused)) line_number)
 {
 	int num = 0;
 	stack_t *aux = *head;
 
-	line_number += 1;
-	line_number -= 1;
-	if (*head == NULL || (*head)->next == NULL)
+	if (*head != NULL || (*head)->next != NULL)
 	{
-		exit(EXIT_FAILURE);
+		while ((*head)->next != NULL)
+		{
+			num = (*head)->n;
+			*head = (*head)->next;
+			(*head)->prev->n = (*head)->n;
+			(*head)->n = num;
+		}
+		*head = aux;
 	}
-	while ((*head)->next != NULL)
-	{
-		num = (*head)->n;
-		*head = (*head)->next;
-		(*head)->prev->n = (*head)->n;
-		(*head)->n = num;
-	}
-	*head = aux;
 }
