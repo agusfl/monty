6a01fe758999a44e3d3035472e1f7b82b2c8ce52
agusfl <agusflom@hotmail.com>
2022-05-11 11:01:29 -0700

    remove comments

--- get_op_func.c
+++ get_op_func.c
@@ -19,10 +19,6 @@ void (*get_op_func(char *token_op))(stack_t **stack, unsigned int line_number)
 		{"mul", _mul},
 		{"mod", _mod},
 		{"nop", nop},
-		/*{"pchar", pchar},
-		{"pstr", pstr},
-		{"rotl", rotl},
-		{"rotr", rotr},*/
 		{NULL, NULL}
 	};
 	int i = 0;
