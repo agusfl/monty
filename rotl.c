618cd066481836c92aca4f34b1316c47c48703c0
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-11 23:38:53 +0000

    rotl

--- rotl.c
+++ rotl.c
@@ -8,10 +8,11 @@
 
 void _rotl(stack_t **head, unsigned int __attribute__((unused)) line_number)
 {
-	stack_t *aux = *head;
+	stack_t *aux;
 
 	if ((*head)->next != NULL)
 	{
+		aux = *head;
 		while (aux->next != NULL)
 			aux = aux->next;
 		aux->next = *head;
@@ -19,6 +20,6 @@ void _rotl(stack_t **head, unsigned int __attribute__((unused)) line_number)
 		(*head)->next->prev = NULL;
 		*head = (*head)->next;
 		aux->next->next = NULL;
-		}
+	}
 }
 
