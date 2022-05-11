68160c4366213cf03c39e2221eb068ab62dc8229
agusfl <agusflom@hotmail.com>
2022-05-11 23:31:38 +0000

    fix new line

--- pstr.c
+++ pstr.c
@@ -9,11 +9,14 @@
 void pstr(stack_t **head, unsigned int __attribute__((unused)) line_number)
 {
 	stack_t *aux = *head;
-
-	while (aux->n != 0 && aux != NULL && aux->n >= 0 && aux->n <= 127)
+	
+	if (*head != NULL)
 	{
-		printf("%c", aux->n); /*usamos el %c para imprimir los num como caracter*/
-		aux = aux->next;
+		while (aux->n != 0 && aux != NULL && aux->n >= 0 && aux->n <= 127)
+		{
+			printf("%c", aux->n); /*usamos el %c para imprimir los num como caracter*/
+			aux = aux->next;
+		}
 	}
 	printf("\n"); /* printeamos el salto de linea despues del string */
 
