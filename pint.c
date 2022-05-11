3f845d00478d075f7e32c2e15f0a22091f968f79
agusfl <agusflom@hotmail.com>
2022-05-11 11:15:45 -0700

    change

--- pint.c
+++ pint.c
@@ -1,19 +1,23 @@
 #include "monty.h"
 
 /**
- * pint - print all nodes in stack from top to bottom.
+ * pint - print the value at the top of the stack, followed by a new line.
  * @head: pointer at the beginning of the list.
  * @line_number: bytecode line number.
  */
 
 void pint(stack_t **head, unsigned int line_number)
 {
+	stack_t *aux = *head;
 
-	if (head == NULL || *head == NULL)
+	if (*head != NULL)
+	{
+		printf("%d\n", aux->n);
+	}
+	else
 	{
 		dprintf(2, "L%u: can't pint, stack empty\n", line_number);
 		/*free(buf);*/
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*head)->n);
 }
