9c1fe56020ed0ec501077731944c18861c88214c
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-12 14:48:47 +0000

    conditional in push

--- bytecodes/12.m
+++ bytecodes/12.m
@@ -1,4 +1,5 @@
-push 1
+push 12a
+pal
 push 2
 push 3
 pall
--- push.c
+++ push.c
@@ -11,7 +11,7 @@ void push(stack_t **head, unsigned int line_number, char *num_str)
 {
 	if (head == NULL)
 		return;
-	if (check_number(num_str) == 2)
+	if (num_str == NULL || check_number(num_str) == 2)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		free_doubly_ll(head);
